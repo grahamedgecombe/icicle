bf6622d9f4715305b6e0c606e9ba723fb17c0628
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-05 23:12:33 +0000

    Add Egyptian multiplication demo program

--- progmem.s
+++ progmem.s
@@ -1,3 +1,19 @@
-start:
-    addi x31, x31, 1
-    j start
+    li t1, 7
+    li t2, 3
+    li t3, 0
+
+# t3 = t1 * t2
+mul:
+    beqz t1, mul_done
+    andi t4, t1, 1
+    beqz t4, mul_even
+    add t3, t3, t2
+mul_even:
+    srli t1, t1, 1
+    slli t2, t2, 1
+    j mul
+
+# display results on the LEDs
+mul_done:
+    mv t6, t3
+    j .
