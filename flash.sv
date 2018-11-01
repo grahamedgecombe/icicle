2c986abb4005c5df1566a8c8d0beee9c2f36b5c8
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-11-01 21:30:40 +0000

    Simplify csn_out logic
    This saves a couple of LUTs.

--- flash.sv
+++ flash.sv
@@ -31,12 +31,11 @@ module flash (
     input [31:0] write_value_in,
     output logic ready_out
 );
-    initial begin
-        csn_out <= 1;
+    initial
         state <= `FLASH_STATE_IDLE;
-    end
 
     assign clk_out = clk;
+    assign csn_out = state != `FLASH_STATE_WRITE_CMD && state != `FLASH_STATE_READ_DATA;
     assign io0_en = 1;
     assign io1_en = 0;
 
@@ -57,7 +56,6 @@ module flash (
         case (state)
             `FLASH_STATE_IDLE: begin
                 if (sel_in && read_in) begin
-                    csn_out <= 0;
                     io0_out <= read_cmd[31];
                     shift_reg <= read_cmd[30:0];
                     state <= `FLASH_STATE_WRITE_CMD;
@@ -80,7 +78,6 @@ module flash (
                     shift_reg <= {shift_reg[29:0], io1_in};
                     bits <= bits - 1;
                 end else begin
-                    csn_out <= 1;
                     read_value <= {shift_reg, io1_in};
                     state <= `FLASH_STATE_DONE;
                 end
@@ -90,10 +87,8 @@ module flash (
             end
         endcase
 
-        if (reset) begin
-            csn_out <= 1;
+        if (reset)
             state <= `FLASH_STATE_IDLE;
-        end
     end
 endmodule
 
