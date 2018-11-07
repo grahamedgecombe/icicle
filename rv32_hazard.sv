d836b9eb56fba539a85ebfc1098fc2ab51605022
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-11-07 17:45:58 +0000

    Fix data memory bus stall logic

--- rv32_hazard.sv
+++ rv32_hazard.sv
@@ -44,24 +44,24 @@ module rv32_hazard_unit (
     logic fetch_wait_for_bus;
     logic fetch_wait_for_mem_read;
     logic fetch_wait_for_mem_fence;
-    logic mem_wait_for_bus;
+    logic execute_wait_for_bus;
 
     assign rs1_matches = decode_rs1_unreg_in == decode_rd_in && decode_rs1_read_unreg_in;
     assign rs2_matches = decode_rs2_unreg_in == decode_rd_in && decode_rs2_read_unreg_in;
     assign fetch_wait_for_bus = instr_read_in && !instr_ready_in;
     assign fetch_wait_for_mem_read = (rs1_matches || rs2_matches) && |decode_rd_in && decode_mem_read_in && decode_rd_write_in;
     assign fetch_wait_for_mem_fence = decode_mem_fence_unreg_in || decode_mem_fence_in || execute_mem_fence_in;
-    assign mem_wait_for_bus = (data_read_in || data_write_in) && !data_ready_in;
+    assign execute_wait_for_bus = (data_read_in || data_write_in) && !data_ready_in;
 
     assign fetch_stall_out = decode_stall_out || fetch_wait_for_mem_read || fetch_wait_for_bus || fetch_wait_for_mem_fence;
 
     assign decode_stall_out = execute_stall_out;
     assign decode_flush_out = fetch_stall_out || mem_branch_mispredicted_in;
 
-    assign execute_stall_out = mem_stall_out;
+    assign execute_stall_out = mem_stall_out || execute_wait_for_bus;
     assign execute_flush_out = decode_stall_out || mem_branch_mispredicted_in;
 
-    assign mem_stall_out = mem_wait_for_bus;
+    assign mem_stall_out = 0;
     assign mem_flush_out = execute_stall_out;
 
     assign writeback_flush_out = mem_stall_out;
