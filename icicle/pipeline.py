c95b45adcfa6982379ca952cd0a4106ad8b451e6
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-12 21:40:27 +0000

    Add pipeline library

---
+++ icicle/pipeline.py
@@ -0,0 +1,67 @@
+from functools import reduce
+from itertools import tee
+from operator import or_
+
+from nmigen import *
+
+VALID_LAYOUT = [
+    ("valid", 1)
+]
+
+
+class Pipeline(Elaboratable):
+    def __init__(self, **stages):
+        self.stages = stages
+
+    def elaborate(self, platform):
+        m = Module()
+
+        for name, stage in self.stages.items():
+            m.submodules[name] = stage
+
+        it1, it2 = tee(self.stages.values())
+        next(it2)
+        for s1, s2 in zip(it1, it2):
+            s1.stall_on(s2.stall)
+            s2.flush_on(s1.stall)
+            m.d.comb += s2.rdata.eq(s1.wdata)
+
+        return m
+
+
+class Stage(Elaboratable):
+    def __init__(self, rdata_layout=None, wdata_layout=None):
+        self.rdata = Record(rdata_layout + VALID_LAYOUT) if rdata_layout is not None else None
+        self.wdata = Record(wdata_layout + VALID_LAYOUT) if wdata_layout is not None else None
+        self.stall = Signal()
+        self.flush = Signal()
+        self.valid = Signal()
+        self._stall_sources = []
+        self._flush_sources = []
+
+    def stall_on(self, source):
+        self._stall_sources.append(source)
+
+    def flush_on(self, source):
+        self._flush_sources.append(source)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.d.comb += [
+            self.stall.eq(reduce(or_, self._stall_sources, 0)),
+            self.flush.eq(reduce(or_, self._flush_sources, 0))
+        ]
+
+        if self.rdata is not None:
+            m.d.comb += self.valid.eq(self.rdata.valid)
+        else:
+            m.d.comb += self.valid.eq(1)
+
+        if self.wdata is not None:
+            with m.If(~self.stall):
+                m.d.sync += self.wdata.valid.eq(self.valid)
+            with m.If(self.flush):
+                m.d.sync += self.wdata.valid.eq(0)
+
+        return m
---
+++ icicle/test/test_pipeline.py
@@ -0,0 +1,159 @@
+from nmigen import *
+from nmigen.back.pysim import Simulator
+from nmigen.test.utils import FHDLTestCase
+
+from icicle.pipeline import Pipeline, Stage
+
+LAYOUT = [
+    ("counter", 32)
+]
+
+
+class FirstStage(Stage):
+    def __init__(self):
+        super().__init__(wdata_layout=LAYOUT)
+
+    def elaborate(self, platform):
+        m = super().elaborate(platform)
+        with m.If(~self.stall):
+            m.d.sync += self.wdata.counter.eq(self.wdata.counter + 1)
+        return m
+
+
+class MiddleStage(Stage):
+    def __init__(self):
+        super().__init__(rdata_layout=LAYOUT, wdata_layout=LAYOUT)
+
+    def elaborate(self, platform):
+        m = super().elaborate(platform)
+        with m.If(~self.stall):
+            m.d.sync += self.wdata.counter.eq(self.rdata.counter)
+        return m
+
+
+class LastStage(Stage):
+    def __init__(self):
+        super().__init__(rdata_layout=LAYOUT)
+        self.counter_valid = Signal()
+        self.counter = Signal(32)
+
+    def elaborate(self, platform):
+        m = super().elaborate(platform)
+        with m.If(~self.stall):
+            m.d.sync += [
+                self.counter_valid.eq(self.valid),
+                self.counter.eq(self.rdata.counter)
+            ]
+        return m
+
+
+class PipelineTestCase(FHDLTestCase):
+    def test_basic(self):
+        s1 = FirstStage()
+        s2 = MiddleStage()
+        s3 = LastStage()
+        m = Pipeline(s1=s1, s2=s2, s3=s3)
+        with Simulator(m) as sim:
+            def process():
+                self.assertEqual((yield s1.wdata.valid), 0)
+                self.assertEqual((yield s2.wdata.valid), 0)
+                self.assertEqual((yield s3.counter_valid), 0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 1)
+                self.assertEqual((yield s2.wdata.valid), 0)
+                self.assertEqual((yield s3.counter_valid), 0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 2)
+                self.assertEqual((yield s2.wdata.valid), 1)
+                self.assertEqual((yield s2.wdata.counter), 1)
+                self.assertEqual((yield s3.counter_valid), 0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 3)
+                self.assertEqual((yield s2.wdata.valid), 1)
+                self.assertEqual((yield s2.wdata.counter), 2)
+                self.assertEqual((yield s3.counter_valid), 1)
+                self.assertEqual((yield s3.counter), 1)
+            sim.add_clock(period=1e-6)
+            sim.add_sync_process(process)
+            sim.run()
+
+    def test_stall(self):
+        s1 = FirstStage()
+        s2 = MiddleStage()
+        s3 = LastStage()
+
+        s2_stall = Signal()
+        s2.stall_on(s2_stall)
+
+        m = Pipeline(s1=s1, s2=s2, s3=s3)
+        with Simulator(m) as sim:
+            def process():
+                yield
+                yield
+                yield s2_stall.eq(1)
+                yield
+                yield s2_stall.eq(0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 3)
+                self.assertEqual((yield s2.wdata.valid), 0)
+                self.assertEqual((yield s3.counter_valid), 1)
+                self.assertEqual((yield s3.counter), 2)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 4)
+                self.assertEqual((yield s2.wdata.valid), 1)
+                self.assertEqual((yield s2.wdata.counter), 3)
+                self.assertEqual((yield s3.counter_valid), 0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 5)
+                self.assertEqual((yield s2.wdata.valid), 1)
+                self.assertEqual((yield s2.wdata.counter), 4)
+                self.assertEqual((yield s3.counter_valid), 1)
+                self.assertEqual((yield s3.counter), 3)
+            sim.add_clock(period=1e-6)
+            sim.add_sync_process(process)
+            sim.run()
+
+    def test_flush(self):
+        s1 = FirstStage()
+        s2 = MiddleStage()
+        s3 = LastStage()
+
+        s2_flush = Signal()
+        s2.flush_on(s2_flush)
+
+        m = Pipeline(s1=s1, s2=s2, s3=s3)
+        with Simulator(m) as sim:
+            def process():
+                yield
+                yield
+                yield s2_flush.eq(1)
+                yield
+                yield s2_flush.eq(0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 4)
+                self.assertEqual((yield s2.wdata.valid), 0)
+                self.assertEqual((yield s3.counter_valid), 1)
+                self.assertEqual((yield s3.counter), 2)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 5)
+                self.assertEqual((yield s2.wdata.valid), 1)
+                self.assertEqual((yield s2.wdata.counter), 4)
+                self.assertEqual((yield s3.counter_valid), 0)
+                yield
+                self.assertEqual((yield s1.wdata.valid), 1)
+                self.assertEqual((yield s1.wdata.counter), 6)
+                self.assertEqual((yield s2.wdata.valid), 1)
+                self.assertEqual((yield s2.wdata.counter), 5)
+                self.assertEqual((yield s3.counter_valid), 1)
+                self.assertEqual((yield s3.counter), 4)
+            sim.add_clock(period=1e-6)
+            sim.add_sync_process(process)
+            sim.run()
