9f711c2e87bf39bb940dd570bdb923f6c75afb5e
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-14 22:21:29 +0000

    Add initial CLI

--- icicle/cli.py
+++ icicle/cli.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+from nmigen.cli import main as nmigen_main
+
+from icicle.cpu import CPU
+
 
 def main():
-    pass
+    nmigen_main(CPU())
 
 
 if __name__ == "__main__":
