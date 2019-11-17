a601a9c6dd33ef0ea7f907b76225f623501d4dcd
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-17 17:38:47 +0000

    Add RVFI record layout

---
+++ icicle/rvfi.py
@@ -0,0 +1,32 @@
+from nmigen.hdl.rec import *
+
+RVFI_LAYOUT = [
+    # instruction metadata
+    ("valid",      1, DIR_FANOUT),
+    ("order",     64, DIR_FANOUT),
+    ("insn",      32, DIR_FANOUT),
+    ("trap",       1, DIR_FANOUT),
+    ("halt",       1, DIR_FANOUT),
+    ("intr",       1, DIR_FANOUT),
+    ("mode",       2, DIR_FANOUT),
+    ("ixl",        2, DIR_FANOUT),
+
+    # integer register read/write
+    ("rs1_addr",   5, DIR_FANOUT),
+    ("rs2_addr",   5, DIR_FANOUT),
+    ("rs1_rdata", 32, DIR_FANOUT),
+    ("rs2_rdata", 32, DIR_FANOUT),
+    ("rd_addr",    5, DIR_FANOUT),
+    ("rd_wdata",  32, DIR_FANOUT),
+
+    # program counter
+    ("pc_rdata",  32, DIR_FANOUT),
+    ("pc_wdata",  32, DIR_FANOUT),
+
+    # memory access
+    ("mem_addr",  32, DIR_FANOUT),
+    ("mem_rmask",  4, DIR_FANOUT),
+    ("mem_wmask",  4, DIR_FANOUT),
+    ("mem_rdata", 32, DIR_FANOUT),
+    ("mem_wdata", 32, DIR_FANOUT)
+]
