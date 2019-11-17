4530686b4056821fc68986170a93e3fb44a1d138
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-17 17:40:20 +0000

    Align tuples in pipeline_regs.py

--- icicle/pipeline_regs.py
+++ icicle/pipeline_regs.py
@@ -3,38 +3,38 @@ PF_LAYOUT = [
 ]
 
 FD_LAYOUT = [
-    ("pc", 32),
+    ("pc",   32),
     ("insn", 32)
 ]
 
 DX_LAYOUT = [
-    ("pc", 32),
-    ("rd", 5),
-    ("rd_wen", 1),
-    ("rs1", 5),
-    ("rs1_ren", 1),
+    ("pc",        32),
+    ("rd",         5),
+    ("rd_wen",     1),
+    ("rs1",        5),
+    ("rs1_ren",    1),
     ("rs1_rdata", 32),
-    ("rs2", 5),
-    ("rs2_ren", 1),
+    ("rs2",        5),
+    ("rs2_ren",    1),
     ("rs2_rdata", 32),
-    ("imm", 32)
+    ("imm",       32)
 ]
 
 XM_LAYOUT = [
-    ("rd", 5),
-    ("rd_wen", 1),
-    ("rs1", 5),
+    ("rd",      5),
+    ("rd_wen",  1),
+    ("rs1",     5),
     ("rs1_ren", 1),
-    ("rs2", 5),
+    ("rs2",     5),
     ("rs2_ren", 1)
 ]
 
 MW_LAYOUT = [
-    ("rd", 5),
-    ("rd_wen", 1),
+    ("rd",        5),
+    ("rd_wen",    1),
     ("rd_wdata", 32),
-    ("rs1", 5),
-    ("rs1_ren", 1),
-    ("rs2", 5),
-    ("rs2_ren", 1)
+    ("rs1",       5),
+    ("rs1_ren",   1),
+    ("rs2",       5),
+    ("rs2_ren",   1)
 ]
