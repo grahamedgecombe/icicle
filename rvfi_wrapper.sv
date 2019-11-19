9d8e81c21f1b65e6ee77437e7b95892df167e73c
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-19 21:09:38 +0000

    Add RVFI wrapper

---
+++ rvfi_wrapper.sv
@@ -0,0 +1,15 @@
+module rvfi_wrapper (
+    input clock,
+    input reset,
+    `RVFI_OUTPUTS
+);
+    \icicle.cpu.CPU #(
+`ifdef `RISCV_FORMAL_BLACKBOX_REGS
+        .rvfi_blackbox_regs(1),
+`endif
+    ) uut (
+        .clk(clock),
+        .rst(reset),
+        `RVFI_CONN
+    );
+endmodule
