d4a657ab1694f0a38fd2008e548926ab016cca0c
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-21 20:52:50 +0000

    Rename sign_extend to arithmetic

--- icicle/shift.py
+++ icicle/shift.py
@@ -4,7 +4,7 @@ from nmigen import *
 class BarrelShifter(Elaboratable):
     def __init__(self):
         self.left = Signal()
-        self.sign_extend = Signal()
+        self.arithmetic = Signal()
         self.a = Signal(32)
         self.shamt = Signal(5)
         self.result = Signal(32)
@@ -18,7 +18,7 @@ class BarrelShifter(Elaboratable):
 
         # convert a to a signed value for arithmetic right shift support
         a_signed = Signal(signed(33))
-        m.d.comb += a_signed.eq(Cat(a_reverse, Mux(self.sign_extend, a_reverse[31], 0)))
+        m.d.comb += a_signed.eq(Cat(a_reverse, Mux(self.arithmetic, a_reverse[31], 0)))
 
         # shift right
         result_reverse = Signal(32)
