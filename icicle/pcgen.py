be69ffcc2bfef163b1fa962db83af0adf2cb9b3c
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-23 10:36:14 +0000

    Add support for taking branches during a pcgen stall

--- icicle/pcgen.py
+++ icicle/pcgen.py
@@ -1,6 +1,5 @@
 from nmigen import *
 
-from icicle.pc import PCMux
 from icicle.pipeline import Stage
 from icicle.pipeline_regs import PF_LAYOUT
 
@@ -8,24 +7,29 @@ from icicle.pipeline_regs import PF_LAYOUT
 class PCGen(Stage):
     def __init__(self, reset_vector):
         super().__init__(wdata_layout=PF_LAYOUT)
-        self.wdata.pc_rdata.reset = reset_vector - 4
+        self.reset_vector = reset_vector
         self.branch_taken = Signal()
         self.branch_target = Signal(32)
 
     def elaborate(self, platform):
         m = super().elaborate(platform)
 
-        pc_mux = m.submodules.pc_mux = PCMux()
-        m.d.comb += [
-            pc_mux.pc_rdata.eq(self.wdata.pc_rdata),
-            pc_mux.branch_taken.eq(self.branch_taken),
-            pc_mux.branch_target.eq(self.branch_target)
-        ]
+        pc = Signal(32, reset=self.reset_vector)
+
+        pc_rdata = Signal(32)
+        with m.If(self.branch_taken):
+            m.d.comb += pc_rdata.eq(self.branch_target)
+        with m.Else():
+            m.d.comb += pc_rdata.eq(pc)
+
+        pc_wdata = Signal(32)
+        m.d.comb += pc_wdata.eq(Mux(self.stall, pc_rdata, pc_rdata + 4))
+        m.d.sync += pc.eq(pc_wdata)
 
         with m.If(~self.stall):
             m.d.sync += [
-                self.wdata.pc_rdata.eq(pc_mux.pc),
-                self.wdata.pc_wdata.eq(pc_mux.pc + 4)
+                self.wdata.pc_rdata.eq(pc_rdata),
+                self.wdata.pc_wdata.eq(pc_wdata)
             ]
 
         return m
--- icicle/test/test_pcgen.py
+++ icicle/test/test_pcgen.py
@@ -1,3 +1,4 @@
+from nmigen import *
 from nmigen.back.pysim import Simulator
 from nmigen.test.utils import FHDLTestCase
 
@@ -19,6 +20,81 @@ class PCGenTestCase(FHDLTestCase):
                 yield
                 self.assertEqual((yield m.wdata.valid), 1)
                 self.assertEqual((yield m.wdata.pc_rdata), 0x00100008)
+            sim.add_clock(period=1e-6)
+            sim.add_sync_process(process)
+            sim.run()
+
+    def test_stall(self):
+        m = PCGen(reset_vector=0x00100000)
+
+        stall = Signal()
+        m.stall_on(stall)
+
+        with Simulator(m) as sim:
+            def process():
+                self.assertEqual((yield m.wdata.valid), 0)
+                yield stall.eq(1)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+                yield stall.eq(0)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00100004)
+            sim.add_clock(period=1e-6)
+            sim.add_sync_process(process)
+            sim.run()
+
+    def test_branch(self):
+        m = PCGen(reset_vector=0x00100000)
+        with Simulator(m) as sim:
+            def process():
+                self.assertEqual((yield m.wdata.valid), 0)
+                yield m.branch_taken.eq(1)
+                yield m.branch_target.eq(0x00200000)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+                yield m.branch_taken.eq(0)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
+            sim.add_clock(period=1e-6)
+            sim.add_sync_process(process)
+            sim.run()
+
+    def test_branch_during_stall(self):
+        m = PCGen(reset_vector=0x00100000)
+
+        stall = Signal()
+        m.stall_on(stall)
+
+        with Simulator(m) as sim:
+            def process():
+                self.assertEqual((yield m.wdata.valid), 0)
+                yield stall.eq(1)
+                yield m.branch_taken.eq(1)
+                yield m.branch_target.eq(0x00200000)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+                yield stall.eq(0)
+                yield m.branch_taken.eq(0)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
+                yield
+                self.assertEqual((yield m.wdata.valid), 1)
+                self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
             sim.add_clock(period=1e-6)
             sim.add_sync_process(process)
             sim.run()
