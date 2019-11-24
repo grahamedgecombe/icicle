81391c8b373ad8481ad4f88875d59ce7dcbd7960
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-11-24 16:24:15 +0000

    Fix stall and flush bugs
    This commit fixes several issues:
    
    * The combinational valid signal didn't take the flush signal into
      account. This caused the processor to hang if a data hazard occurred,
      as the decode stage would stall forever if the instruction conflicted
      with a pipeline bubble in the execute stage, as execute.valid was
      always one (even though it would write zeroes to execute.wdata.valid).
    
    * Flushes now take precedence over stalls. Previously, stalls took
      precedence over flushes. If a taken JALR in the mem stage writing to a
      particular register was followed by an instruction reading from the
      the same register in the decode stage, the decode stage would be
      simultaneously stalled and flushed. The stall would take predence, so
      the instruction would remain in the decode stage until the next cycle.
      However, the flush would be lowered on the next cycle (as the
      instruction in the mem stage would have moved to the writeback stage),
      causing the instruction in the decode stage to incorrectly proceed to
      the execute stage.
    
    * A non-stalled stage following a stalled stage would be incorrectly
      flushed during the first cycle of the stall, causing an instruction to
      be dropped. The flush only needs to occur during the following cycle.

--- icicle/pipeline.py
+++ icicle/pipeline.py
@@ -22,9 +22,10 @@ class Pipeline(Elaboratable):
         it1, it2 = tee(self.stages.values())
         next(it2)
         for s1, s2 in zip(it1, it2):
-            s1.stall_on(s2.stall)
-            s2.flush_on(s1.stall & ~s2.stall)
-            m.d.comb += s2.rdata.eq(s1.wdata)
+            m.d.comb += [
+                s2.rdata.eq(s1.wdata),
+                s1.next_stall.eq(s2.stall)
+            ]
 
         return m
 
@@ -36,6 +37,7 @@ class Stage(Elaboratable):
         if wdata_layout:
             self.wdata = Record(wdata_layout + VALID_LAYOUT)
         self.stall = Signal()
+        self.next_stall = Signal()
         self.flush = Signal()
         self.valid = Signal()
         self._stall_sources = []
@@ -51,19 +53,17 @@ class Stage(Elaboratable):
         m = Module()
 
         m.d.comb += [
-            self.stall.eq(reduce(or_, self._stall_sources, 0)),
+            self.stall.eq((reduce(or_, self._stall_sources, 0) | self.next_stall) & ~self.flush),
             self.flush.eq(reduce(or_, self._flush_sources, 0))
         ]
 
-        if hasattr(self, "rdata"):
-            m.d.comb += self.valid.eq(self.rdata.valid)
-        else:
-            m.d.comb += self.valid.eq(1)
+        rdata_valid = self.rdata.valid if hasattr(self, "rdata") else 1
+        m.d.comb += self.valid.eq(rdata_valid & ~self.flush)
 
         if hasattr(self, "wdata"):
             with m.If(~self.stall):
                 m.d.sync += self.wdata.valid.eq(self.valid)
-            with m.If(self.flush):
+            with m.Elif(~self.next_stall):
                 m.d.sync += self.wdata.valid.eq(0)
 
         if hasattr(self, "rdata") and hasattr(self, "wdata"):
