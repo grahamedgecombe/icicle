03f39b8a54f0ae5469c6136bf7ae490870fa5b1b
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-04 23:38:18 +0000

    Ensure the subtract/SRA bit is zero if funct3 is SLL
    This fixes the complete test, so all the riscv-formal tests we currently
    support now pass!

--- icicle/control.py
+++ icicle/control.py
@@ -197,10 +197,12 @@ class Control(Elaboratable):
                         # If nMigen adds support for "don't care" bits we could
                         # tidy this up.
                     with m.Case(Funct3.SLL, Funct3.SRL_SRA):
-                        m.d.comb += [
-                            self.result_sel.eq(ResultSel.SHIFT),
-                            self.illegal.eq(~funct7.matches(Funct7.ZERO, Funct7.SUB_SRA))
-                        ]
+                        m.d.comb += self.result_sel.eq(ResultSel.SHIFT)
+
+                        with m.If(funct3 == Funct3.SLL):
+                            m.d.comb += self.illegal.eq(funct7 != Funct7.ZERO)
+                        with m.Else():
+                            m.d.comb += self.illegal.eq(~funct7.matches(Funct7.ZERO, Funct7.SUB_SRA))
                         # XXX(gpe): we set shift_right and shift_arithmetic
                         # below as they always occupy the same bits in funct3
                         # and funct7. As above, this could be tidied up with
