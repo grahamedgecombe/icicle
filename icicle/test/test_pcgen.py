f4f2ab9a26dd34be6400f609f37f8dac73b11240
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-07 18:52:49 +0000

    Add pc_wdata and intr to the PCGen tests
    I think testing these is still useful, even though they're only required
    for the RVFI port.

--- icicle/test/test_pcgen.py
+++ icicle/test/test_pcgen.py
@@ -14,12 +14,18 @@ class PCGenTestCase(FHDLTestCase):
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100004)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100008)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100008)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x0010000C)
+            self.assertEqual((yield m.wdata.intr), 0)
         sim.add_clock(period=1e-6)
         sim.add_sync_process(process)
         sim.run()
@@ -37,13 +43,19 @@ class PCGenTestCase(FHDLTestCase):
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield stall.eq(0)
             yield
             self.assertEqual((yield m.wdata.valid), 0)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100004)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100008)
+            self.assertEqual((yield m.wdata.intr), 0)
         sim.add_clock(period=1e-6)
         sim.add_sync_process(process)
         sim.run()
@@ -58,13 +70,19 @@ class PCGenTestCase(FHDLTestCase):
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield m.branch_taken.eq(0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200008)
+            self.assertEqual((yield m.wdata.intr), 0)
         sim.add_clock(period=1e-6)
         sim.add_sync_process(process)
         sim.run()
@@ -84,17 +102,25 @@ class PCGenTestCase(FHDLTestCase):
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield stall.eq(0)
             yield m.branch_taken.eq(0)
             yield
             self.assertEqual((yield m.wdata.valid), 0)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200008)
+            self.assertEqual((yield m.wdata.intr), 0)
         sim.add_clock(period=1e-6)
         sim.add_sync_process(process)
         sim.run()
@@ -108,13 +134,19 @@ class PCGenTestCase(FHDLTestCase):
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield m.trap_raised.eq(0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200004)
+            self.assertEqual((yield m.wdata.intr), 1)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200008)
+            self.assertEqual((yield m.wdata.intr), 0)
         sim.add_clock(period=1e-6)
         sim.add_sync_process(process)
         sim.run()
@@ -133,17 +165,25 @@ class PCGenTestCase(FHDLTestCase):
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield stall.eq(0)
             yield m.trap_raised.eq(0)
             yield
             self.assertEqual((yield m.wdata.valid), 0)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
+            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200004)
+            self.assertEqual((yield m.wdata.intr), 1)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
+            self.assertEqual((yield m.wdata.pc_wdata), 0x00200008)
+            self.assertEqual((yield m.wdata.intr), 0)
         sim.add_clock(period=1e-6)
         sim.add_sync_process(process)
         sim.run()
