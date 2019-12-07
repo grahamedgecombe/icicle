6f5b28192da7793850f6693b851afe6352de75bf
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-07 19:57:23 +0000

    Optimize PCGen stage

--- icicle/pcgen.py
+++ icicle/pcgen.py
@@ -7,48 +7,38 @@ from icicle.pipeline_regs import PF_LAYOUT
 class PCGen(Stage):
     def __init__(self, reset_vector=0, trap_vector=0):
         super().__init__(wdata_layout=PF_LAYOUT)
-        self.reset_vector = reset_vector
+        self.wdata.pc_rdata.reset = reset_vector - 4
         self.trap_vector = trap_vector
         self.branch_taken = Signal()
         self.branch_target = Signal(32)
         self.trap_raised = Signal()
 
     def elaborate_stage(self, m, platform):
-        pc = Signal(32, reset=self.reset_vector)
-        intr = Signal()
-
-        pc_rdata = Signal(32)
-        intr_rdata = Signal()
+        # XXX(gpe): it's impossible for stall to be set at the same time as
+        # branch_taken or trap_raised. The PC generation stage never stalls
+        # itself. The fetch stage is always flushed when branch_taken or
+        # trap_raised is set. Flushes take precedence over stalls, so this
+        # stage can't be stalled due to the next stage being stalled, provided
+        # branch_taken or trap_raised is set.
+        #
+        # This allows us to optimise branches/traps slightly: we can write
+        # directly to wdata.pc_rdata, instead of writing to a separate register
+        # and then copying to wdata.pc_rdata in the ~self.stall case.
         with m.If(self.branch_taken):
-            m.d.comb += [
-                pc_rdata.eq(self.branch_target),
-                intr_rdata.eq(0)
+            m.d.sync += [
+                self.wdata.pc_rdata.eq(self.branch_target),
+                self.wdata.pc_wdata.eq(self.branch_target + 4),
+                self.wdata.intr.eq(0)
             ]
         with m.Elif(self.trap_raised):
-            m.d.comb += [
-                pc_rdata.eq(self.trap_vector),
-                intr_rdata.eq(1)
-            ]
-        with m.Else():
-            m.d.comb += [
-                pc_rdata.eq(pc),
-                intr_rdata.eq(intr)
-            ]
-
-        pc_wdata = Signal(32)
-        intr_wdata = Signal()
-        m.d.comb += [
-            pc_wdata.eq(Mux(self.stall, pc_rdata, pc_rdata + 4)),
-            intr_wdata.eq(Mux(self.stall, intr_rdata, 0))
-        ]
-        m.d.sync += [
-            pc.eq(pc_wdata),
-            intr.eq(intr_wdata)
-        ]
-
-        with m.If(~self.stall):
             m.d.sync += [
-                self.wdata.pc_rdata.eq(pc_rdata),
-                self.wdata.pc_wdata.eq(pc_wdata),
-                self.wdata.intr.eq(intr_rdata)
+                self.wdata.pc_rdata.eq(self.trap_vector),
+                self.wdata.pc_wdata.eq(self.trap_vector + 4),
+                self.wdata.intr.eq(1)
+            ]
+        with m.Elif(~self.stall):
+            m.d.sync += [
+                self.wdata.pc_rdata.eq(self.wdata.pc_rdata + 4),
+                self.wdata.pc_wdata.eq(self.wdata.pc_rdata + 8),
+                self.wdata.intr.eq(0)
             ]
--- icicle/test/test_pcgen.py
+++ icicle/test/test_pcgen.py
@@ -87,44 +87,6 @@ class PCGenTestCase(FHDLTestCase):
         sim.add_sync_process(process)
         sim.run()
 
-    def test_branch_during_stall(self):
-        m = PCGen(reset_vector=0x00100000)
-
-        stall = Signal()
-        m.stall_on(stall)
-
-        sim = Simulator(m)
-        def process():
-            self.assertEqual((yield m.wdata.valid), 0)
-            yield stall.eq(1)
-            yield m.branch_taken.eq(1)
-            yield m.branch_target.eq(0x00200000)
-            yield
-            self.assertEqual((yield m.wdata.valid), 1)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
-            self.assertEqual((yield m.wdata.intr), 0)
-            yield stall.eq(0)
-            yield m.branch_taken.eq(0)
-            yield
-            self.assertEqual((yield m.wdata.valid), 0)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
-            self.assertEqual((yield m.wdata.intr), 0)
-            yield
-            self.assertEqual((yield m.wdata.valid), 1)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00200004)
-            self.assertEqual((yield m.wdata.intr), 0)
-            yield
-            self.assertEqual((yield m.wdata.valid), 1)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00200008)
-            self.assertEqual((yield m.wdata.intr), 0)
-        sim.add_clock(period=1e-6)
-        sim.add_sync_process(process)
-        sim.run()
-
     def test_trap(self):
         m = PCGen(reset_vector=0x00100000, trap_vector=0x00200000)
         sim = Simulator(m)
@@ -137,43 +99,6 @@ class PCGenTestCase(FHDLTestCase):
             self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
             self.assertEqual((yield m.wdata.intr), 0)
             yield m.trap_raised.eq(0)
-            yield
-            self.assertEqual((yield m.wdata.valid), 1)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00200004)
-            self.assertEqual((yield m.wdata.intr), 1)
-            yield
-            self.assertEqual((yield m.wdata.valid), 1)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00200004)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00200008)
-            self.assertEqual((yield m.wdata.intr), 0)
-        sim.add_clock(period=1e-6)
-        sim.add_sync_process(process)
-        sim.run()
-
-    def test_trap_during_stall(self):
-        m = PCGen(reset_vector=0x00100000, trap_vector=0x00200000)
-
-        stall = Signal()
-        m.stall_on(stall)
-
-        sim = Simulator(m)
-        def process():
-            self.assertEqual((yield m.wdata.valid), 0)
-            yield stall.eq(1)
-            yield m.trap_raised.eq(1)
-            yield
-            self.assertEqual((yield m.wdata.valid), 1)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
-            self.assertEqual((yield m.wdata.intr), 0)
-            yield stall.eq(0)
-            yield m.trap_raised.eq(0)
-            yield
-            self.assertEqual((yield m.wdata.valid), 0)
-            self.assertEqual((yield m.wdata.pc_rdata), 0x00100000)
-            self.assertEqual((yield m.wdata.pc_wdata), 0x00100004)
-            self.assertEqual((yield m.wdata.intr), 0)
             yield
             self.assertEqual((yield m.wdata.valid), 1)
             self.assertEqual((yield m.wdata.pc_rdata), 0x00200000)
