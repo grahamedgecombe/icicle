60cdcedbf22324c6c6a0b7dca05504974c7f7c05
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-10 23:10:30 +0000

    Add address decoder

--- icicle/wishbone.py
+++ icicle/wishbone.py
@@ -1,3 +1,4 @@
+from nmigen import *
 from nmigen.hdl.rec import *
 
 WISHBONE_LAYOUT = [
@@ -11,3 +12,38 @@ WISHBONE_LAYOUT = [
     ("ack",    1, DIR_FANIN),
     ("err",    1, DIR_FANIN)
 ]
+
+
+class AddrDecoder(Elaboratable):
+    def __init__(self, slaves):
+        self.master = Record(WISHBONE_LAYOUT)
+        self.slaves = slaves
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # fault if none of the slaves are selected below
+        m.d.comb += self.master.err.eq(1)
+
+        for (addr, slave) in self.slaves:
+            select = self.master.adr.matches(addr)
+
+            # fan-in
+            with m.If(select):
+                m.d.comb += [
+                    self.master.dat_r.eq(slave.dat_r),
+                    self.master.ack.eq(slave.ack),
+                    self.master.err.eq(slave.err)
+                ]
+
+            # fan-out
+            m.d.comb += [
+                slave.adr.eq(self.master.adr),
+                slave.dat_w.eq(self.master.dat_w),
+                slave.sel.eq(self.master.sel),
+                slave.cyc.eq(self.master.cyc & select),
+                slave.stb.eq(self.master.stb & select),
+                slave.we.eq(self.master.we)
+            ]
+
+        return m
