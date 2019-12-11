9ac7220aa7be543574272f82663633b040bcf0ed
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-11 21:57:10 +0000

    Use valid_before instead of valid in data_hazard()
    This reduces the critical path slightly, in theory at the expense of
    stalling the decode stage if a trap occurs. However, in reality, if a
    trap does occur the instruction in the decode stage will end up being
    flushed once the trap reaches the memory access stage, so I don't think
    this has a CPI impact in practice.

--- icicle/cpu.py
+++ icicle/cpu.py
@@ -58,7 +58,7 @@ class CPU(Elaboratable):
         def data_hazard(stage):
             rs1_matches = decode.rs1_ren & (decode.rs1_port.addr == stage.rdata.rd)
             rs2_matches = decode.rs2_ren & (decode.rs2_port.addr == stage.rdata.rd)
-            return stage.valid & stage.rdata.rd_wen & (rs1_matches | rs2_matches)
+            return stage.valid_before & stage.rdata.rd_wen & (rs1_matches | rs2_matches)
 
         decode.stall_on(data_hazard(execute))
         decode.stall_on(data_hazard(mem))
