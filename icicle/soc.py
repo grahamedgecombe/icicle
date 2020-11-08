4c0367c868b48d16ad7934c7fe2f28d87a694c56
Graham Edgecombe <gpe@grahamedgecombe.com>
2020-11-08 22:05:46 +0000

    Store GPIO memory address in a register in the sample program
    This will allow a non-zero GPIO address (to avoid conflicting with the
    address of the program itself) to be used when the instruction and data
    buses are combined in a future commit.

--- icicle/soc.py
+++ icicle/soc.py
@@ -16,19 +16,21 @@ class SystemOnChip(Elaboratable):
             #   li t0, 1000000
             0x000f42b7,
             0x24028293,
+            #   li t1, 0
+            0x00000313,
             # dec:
             #   addi t0, t0, -1
             0xfff28293,
-            #   bne t0, zero, dec
+            #   bnez t0, dec
             0xfe029ee3,
-            #   lw t1, 0(zero)
-            0x00002303,
-            #   xori t1, t1, 1
-            0x00134313,
-            #   sw t1, 0(zero)
-            0x00602023,
+            #   lw t1, 0(t1)
+            0x00032383,
+            #   xori t2, t2, 1
+            0x0013c393,
+            #   sw t1, 0(t1)
+            0x00732023,
             #   j start
-            0xfe5ff06f
+            0xfe1ff06f
         ])
         m.d.comb += cpu.ibus.connect(bram.bus)
 
