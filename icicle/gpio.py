aede6eec14aef81cce381353166a30f1b54cc1ad
Graham Edgecombe <gpe@grahamedgecombe.com>
2020-11-10 22:06:33 +0000

    Remove err feature from peripherals
    nMigen automatically sets err to 0 if a peripheral doesn't have the err
    feature when the peripheral is connected to another bus.

--- icicle/gpio.py
+++ icicle/gpio.py
@@ -5,7 +5,7 @@ from nmigen_soc.memory import MemoryMap
 
 class GPIO(Elaboratable):
     def __init__(self):
-        self.bus = wishbone.Interface(addr_width=0, data_width=32, granularity=8, features=["err"])
+        self.bus = wishbone.Interface(addr_width=0, data_width=32, granularity=8)
         self.bus.memory_map = MemoryMap(addr_width=2, data_width=8)
 
     def elaborate(self, platform):
